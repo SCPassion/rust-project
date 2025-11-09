e3b9da2f526c15906240bdbd72540bd438e03fec
scp <bernard.cryptohk@gmail.com>
2025-11-09 20:31:35 +0100

    Enhance Book struct visibility and improve documentation in structs.rs for better understanding.

---
+++ src/lessons/panics.rs
@@ -0,0 +1,20 @@
+fn panics() {
+    let veggies = ["carrots", "tomatoes", "potatoes"];
+    chooseVeggie(veggies[0]);
+    chooseVeggie(veggies[1]);
+    chooseVeggie(veggies[2]);
+    chooseVeggie("onions");
+}
+
+fn chooseVeggie(veggie: &str) {
+    match veggie {
+        "carrots" => println!("I love carrots"),
+        "tomatoes" => println!("I love tomatoes"),
+        "potatoes" => println!("I love potatoes"),
+        _ => panic!("This is not an acceptable veggie"), // This is a panic, saying that this is not an acceptable veggie.
+                                                         // If hitted, the program will not continue and crash.
+    }
+}
+
+// If you dont' want the program to run, when somthing happens. You can use panic! to stop the program.
+// If you use Result, with a proper Err, you can handle the error and continue the program.
